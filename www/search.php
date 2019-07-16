1bbcd8701aae39a73e5405e9e9e32411b7ad8f9d
acien101 <amil101ftw@gmail.com>
2019-07-16 16:24:59 +0200

    adding modals to search page

--- www/search.php
+++ www/search.php
@@ -126,5 +126,41 @@ END;
           });
       });
   </script>
+
+  <!-- Modal -->
+  <div class="modal fade" id="imageModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-md">
+              <img class="card-img-top" src="#" alt="">
+            </div>
+            <div class="col-sm">
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item"><b>Date uploaded: </b> <a id="date-uploaded"></a></li>
+                <li class="list-group-item"><b>Telecope:</b> <a id="telescope"></a> </li>
+                <li class="list-group-item"><b>Camera:</b> <a id="instrume"></a></li>
+                <li class="list-group-item"><b>Time exposure:</b> <a id="exposure"></a></li>
+                <li class="list-group-item"><b>Latitude:</b> <a id="lat"></a></li>
+                <li class="list-group-item"><b>Longitude:</b> <a id="long"></a></li>
+                <li class="list-group-item"><b>Altitude:</b> <a id="alt"></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <a href="#" class="btn btn-primary" tabindex="-1" role="button" aria-disabled="true">More info</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
