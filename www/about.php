3d1a9d05f87bf14e01bd90dfd55e61e77ddfad7f
Francisco Jesús Acién Pérez <f.acien@upm.es>
2019-07-25 16:27:08 +0200

    adding about sample code

---
+++ www/about.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include './assets/partials/head.php' ?>
+</head>
+<body>
+  <!-- Navigation -->
+  <?php include './assets/partials/navbar.php'?>
+
+  <!-- Page Content -->
+  <div class="container">
+    <h1>About Us</h1>
+
+    <img style='float:left;width:200px;height:200px; margin-right:10px;' src="img/me.jpg" />
+    <p>Lots of text here...With the four tiers of grids available you're bound to run into issues where, at certain breakpoints, your columns don't clear quite right as one is taller than the other. To fix that, use a combination of a .clearfix and o</p>
+    <p>Lots of text here...With the four tiers of grids available you're bound to run into issues where, at certain breakpoints, your columns don't clear quite right as one is taller than the other. To fix that, use a combination of a .clearfix and o</p>
+
+    <p>Lots of text here...With the four tiers of grids available you're bound to run into issues where, at certain breakpoints, your columns don't clear quite right as one is taller than the other. To fix that, use a combination of a .clearfix and o</p>
+
+    <p>Lots of text here...With the four tiers of grids available you're bound to run into issues where, at certain breakpoints, your columns don't clear quite right as one is taller than the other. To fix that, use a combination of a .clearfix and o</p>
+    <p>Lots of text here...With the four tiers of grids available you're bound to run into issues where, at certain breakpoints, your columns don't clear quite right as one is taller than the other. To fix that, use a combination of a .clearfix and o</p>
+
+  </div>
+  <!-- /.container -->
+  <!-- Footer -->
+  <?php include './assets/partials/footer.php'?>
+
+  <!-- Modal -->
+  <div class="modal fade" id="imageModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-md">
+              <img class="card-img-top" src="#" alt="">
+            </div>
+            <div class="col-sm">
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item"><b>Date uploaded: </b> <a id="date-uploaded"></a></li>
+                <li class="list-group-item"><b>Telecope:</b> <a id="telescope"></a> </li>
+                <li class="list-group-item"><b>Filter:</b> <a id="filter"></a> </li>
+                <li class="list-group-item"><b>Camera:</b> <a id="instrume"></a></li>
+                <li class="list-group-item"><b>Time exposure:</b> <a id="exposure"></a></li>
+                <li class="list-group-item"><b>Latitude:</b> <a id="lat"></a></li>
+                <li class="list-group-item"><b>Longitude:</b> <a id="long"></a></li>
+                <li class="list-group-item"><b>Altitude:</b> <a id="alt"></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <a href="#" class="btn btn-primary" tabindex="-1" role="button" aria-disabled="true">More info</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
