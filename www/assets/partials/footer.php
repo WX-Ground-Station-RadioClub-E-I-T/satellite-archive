81ff16c2b0c35567b7bd1f8d9fff729cb6a32ae4
acien101 <f.acien@upm.es>
2019-08-12 16:47:53 +0200

    update year on footer

--- www/assets/partials/footer.php
+++ www/assets/partials/footer.php
@@ -5,7 +5,7 @@
         <tr>
           <td class="text-center ">
             <span>Terms of Use | Privacy Policy | Legal Notice<br>
-            <span class="footer_secondary">Copyright 2011 - 2019 © CESAR - Cooperation through Education in Science and Astronomy Research</span></span><br>
+            <span class="footer_secondary">Copyright 2011 - <?php echo date("Y"); ?> © CESAR - Cooperation through Education in Science and Astronomy Research</span></span><br>
           </td>
         </tr>
       </tbody>
